c5e80e0f9f5cf951f91aa9828315869a1dc0cec4
namodynamic <nnamdi4u09@gmail.com>
2023-10-29 18:52:28 +0100

    first commit

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,9 @@
+import * as z from "zod"
+
+export const SignupValidation = z.object({
+    name: z.string().min(2,{message: "Name should be at least 2 characters"}),
+    username: z.string().min(2,{message: "Username should be at least 2 characters"}),
+    email: z.string().email({ message: "Enter a valid email" }),
+    password: z.string().min(8,{message: "Password should be at least 8 characters"}),
+  })
+  
