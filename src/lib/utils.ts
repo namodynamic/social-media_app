0e829ff692667add5fc801037112b8e696012d8e
namodynamic <nnamdi4u09@gmail.com>
2023-11-02 18:41:30 +0100

    first commit

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,29 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDateToRelative(dateString: string): string {
+  const date = new Date(dateString);
+  const now = new Date();
+
+  const timeDifferenceInSeconds = Math.floor((now.getTime() - date.getTime()) / 1000);
+
+  if (timeDifferenceInSeconds < 60) {
+    return 'Just now';
+  } else if (timeDifferenceInSeconds < 3600) {
+    const minutesAgo = Math.floor(timeDifferenceInSeconds / 60);
+    return `${minutesAgo} minute${minutesAgo === 1 ? '' : 's'} ago`;
+  } else if (timeDifferenceInSeconds < 86400) {
+    const hoursAgo = Math.floor(timeDifferenceInSeconds / 3600);
+    return `${hoursAgo} hour${hoursAgo === 1 ? '' : 's'} ago`;
+  } else {
+    const daysAgo = Math.floor(timeDifferenceInSeconds / 86400);
+    return `${daysAgo} day${daysAgo === 1 ? '' : 's'} ago`;
+  }
+}
+
+
+export const checkIsLiked = (likeList: string[], userId: string) => {
+  return likeList.includes(userId);
+};
+
