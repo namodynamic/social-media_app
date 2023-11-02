6c2c26399d03a4faf0f868b08e8c332d7785e6ec
namodynamic <nnamdi4u09@gmail.com>
2023-11-02 19:15:23 +0100

    initial commit

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Models } from "appwrite";
+
+import Loader from "@/components/shared/Loader";
+import { useGetRecentPosts } from "@/lib/react-query/querriesAndMutations";
+import PostCard from "@/components/shared/PostCard";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const {
+    data: posts,
+    isPending: isPostLoading,
+    isError: isErrorPosts,
+  } = useGetRecentPosts();
 
-export default Home
+  return (
+    <div className="flex flex-1">
+      <div className="home-container">
+        <div className="home-posts">
+          <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+          {isPostLoading && !posts ? (
+            <Loader />
+          ) : (
+            <ul className="flex flex-col flex-1 gap-9 w-full">
+              {posts?.documents.map((post: Models.Document) => (
+                <PostCard post={post} key={post.caption} />
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
